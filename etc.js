9b2bc1d78d48c6c4c6683d2524e4cc1206eec847
x15-rahul <biswasrahul97@outlook.com>
2023-03-29 16:11:49 +0600

    first file is added

---
+++ etc.js
@@ -0,0 +1,28 @@
+const mySelf = {
+    firstName: 'Rahul',
+    lastName: 'Biswas',
+    age: 25,
+    isRich: false
+};
+
+function printName(name){
+    console.log(name);
+}
+
+const friendsAge = [21, 23, 24, 25, 26];
+const newFriendsAge = [29, 31, 35, 28];
+
+// console.log(typeof mySelf);
+// console.log(typeof printName);
+// console.log(Array.isArray(friendsAge));
+
+// console.log(friendsAge.indexOf(21));
+/* if(friendsAge.includes(20)){
+    console.log('25 is here');
+} else {
+    console.log('25 is not here');
+} */
+
+const newArray = friendsAge.concat(newFriendsAge);
+console.log(newArray.join('|'));
+
