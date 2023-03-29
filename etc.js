56d08ea719782a959fefbf59f1f2dbd21dcf5158
x15-rahul <biswasrahul97@outlook.com>
2023-03-29 16:13:47 +0600

    some code inside file is changed

--- etc.js
+++ etc.js
@@ -12,16 +12,16 @@ function printName(name){
 const friendsAge = [21, 23, 24, 25, 26];
 const newFriendsAge = [29, 31, 35, 28];
 
-// console.log(typeof mySelf);
-// console.log(typeof printName);
-// console.log(Array.isArray(friendsAge));
+console.log(typeof mySelf);
+console.log(typeof printName);
+console.log(Array.isArray(friendsAge));
 
-// console.log(friendsAge.indexOf(21));
-/* if(friendsAge.includes(20)){
+console.log(friendsAge.indexOf(21));
+if(friendsAge.includes(20)){
     console.log('25 is here');
 } else {
     console.log('25 is not here');
-} */
+}
 
 const newArray = friendsAge.concat(newFriendsAge);
 console.log(newArray.join('|'));
